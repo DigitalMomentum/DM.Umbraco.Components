f087516f5a9f94e70c744f5de585c1c1d327ac22
David Sheiles <david@sheiles.com>
2018-03-26 21:21:19 +1100

    Update assembly name

--- DM.Umbraco.Components.ImageButton/DM.Umbraco.Components.ImageButton/Properties/AssemblyInfo.cs
+++ DM.Umbraco.Components.ImageButton/DM.Umbraco.Components.ImageButton/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DM.Umbraco.Components.ImageButton")]
+[assembly: AssemblyTitle("DigitalMomentum.Umbraco.ImageButton")]
 [assembly: AssemblyDescription("An Umbraco Datatype/Grid Editor to add an image with text overlay + link to node/media")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Digital Momentum")]
