ad5507d2e5a1c7ca8519bc6ea6e8f11cc121b87e
David Sheiles <david@digitalmomentum.com.au>
2018-03-31 23:56:42 +1100

    Initial Checkin. Not Working yet

---
+++ DM.Umbraco.Components.MemberLogin/DM.Umbraco.Components.MemberLogin/Controllers/Members/MembershipController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace DM.Components.MemberLogin.Controllers.Members {
+	public class MembershipController : Umbraco.Web.Mvc.SurfaceController {
+		[HttpGet]
+		[ActionName("MemberLogin")]
+		public ActionResult Index() {
+			return PartialView("Membership/MemberLogin", new LoginModel());
+		}
+
+		[HttpGet]
+		public ActionResult Logout() {
+			Session.Clear();
+			FormsAuthentication.SignOut();
+			return Redirect("/");
+		}
+
+		[HttpPost]
+		[ActionName("MemberLogin")]
+		public ActionResult Validate(LoginModel model) {
+			if (Membership.ValidateUser(model.Login, model.Password)) {
+				FormsAuthentication.SetAuthCookie(model.Login, model.RememberMe);
+				Response.Redirect(Request.RawUrl);
+			}
+
+			TempData["Status"] = "Invalid Log-in Credentials";
+			return RedirectToCurrentUmbracoPage();
+		}
+	}
+}
---
+++ DM.Umbraco.Components.MemberLogin/DM.Umbraco.Components.MemberLogin/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DM.Umbraco.Components.MemberLogin")]
+[assembly: AssemblyDescription("Add's Member login forms and member handling for front endUmbraco Sites")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Digital Momentum")]
+[assembly: AssemblyProduct("DM.Umbraco.Components.MemberLogin")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1323a3fc-7d67-4f4b-93ac-f30ab1b51a69")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
